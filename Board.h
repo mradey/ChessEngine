09ac66d6aec42dd84b0b257fd740145c3ce17fec
Michael James Radey <radeymichael@gmail.com>
2020-12-02 10:46:50 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -15,46 +15,51 @@ class Board {
         Board() {
             resetBoard();
         }
+
+        enum columns { A, B, C, D, E, F, G, H };
+
         void resetBoard() {
             //white pieces
-            board[7][0] = new Square(7,0,new Rook(true));
-            board[7][1] = new Square(7,1,new Knight(true));
-            board[7][2] = new Square(7,2,new Bishop(true));
-            board[7][3] = new Square(7,3,new Queen(true));
-            board[7][4] = new Square(7,4,new King(true));
-            board[7][5] = new Square(7,5,new Bishop(true));
-            board[7][6] = new Square(7,6,new Knight(true));
-            board[7][7] = new Square(7,7,new Rook(true));
+            bool isWhite = true;
+            board[7][A] = new Square(7,A,new Rook(isWhite));
+            board[7][B] = new Square(7,B,new Knight(isWhite));
+            board[7][C] = new Square(7,C,new Bishop(isWhite));
+            board[7][D] = new Square(7,D,new Queen(isWhite));
+            board[7][E] = new Square(7,E,new King(isWhite));
+            board[7][F] = new Square(7,F,new Bishop(isWhite));
+            board[7][G] = new Square(7,G,new Knight(isWhite));
+            board[7][H] = new Square(7,H,new Rook(isWhite));
 
             //white pawns
-            board[6][0] = new Square(6,0,new Pawn(true));
-            board[6][1] = new Square(6,1,new Pawn(true));
-            board[6][2] = new Square(6,2,new Pawn(true));
-            board[6][3] = new Square(6,3,new Pawn(true));
-            board[6][4] = new Square(6,4,new Pawn(true));
-            board[6][5] = new Square(6,5,new Pawn(true));
-            board[6][6] = new Square(6,6,new Pawn(true));
-            board[6][7] = new Square(6,7,new Pawn(true));
+            board[6][A] = new Square(6,A,new Pawn(isWhite));
+            board[6][B] = new Square(6,B,new Pawn(isWhite));
+            board[6][C] = new Square(6,C,new Pawn(isWhite));
+            board[6][D] = new Square(6,D,new Pawn(isWhite));
+            board[6][E] = new Square(6,E,new Pawn(isWhite));
+            board[6][F] = new Square(6,F,new Pawn(isWhite));
+            board[6][G] = new Square(6,G,new Pawn(isWhite));
+            board[6][H] = new Square(6,H,new Pawn(isWhite));
 
             //black pieces
-            board[0][0] = new Square(0,0,new Rook(false));
-            board[0][1] = new Square(0,1,new Knight(false));
-            board[0][2] = new Square(0,2,new Bishop(false));
-            board[0][3] = new Square(0,3,new Queen(false));
-            board[0][4] = new Square(0,4,new King(false));
-            board[0][5] = new Square(0,5,new Bishop(false));
-            board[0][6] = new Square(0,6,new Knight(false));
-            board[0][7] = new Square(0,7,new Rook(false));
+            bool isWhite = false;
+            board[0][A] = new Square(0,A,new Rook(isWhite));
+            board[0][B] = new Square(0,B,new Knight(isWhite));
+            board[0][C] = new Square(0,C,new Bishop(isWhite));
+            board[0][D] = new Square(0,D,new Queen(isWhite));
+            board[0][E] = new Square(0,E,new King(isWhite));
+            board[0][F] = new Square(0,F,new Bishop(isWhite));
+            board[0][G] = new Square(0,G,new Knight(isWhite));
+            board[0][H] = new Square(0,H,new Rook(isWhite));
 
             //black pawns
-            board[1][0] = new Square(1,0,new Pawn(false));
-            board[1][1] = new Square(1,1,new Pawn(false));
-            board[1][2] = new Square(1,2,new Pawn(false));
-            board[1][3] = new Square(1,3,new Pawn(false));
-            board[1][4] = new Square(1,4,new Pawn(false));
-            board[1][5] = new Square(1,5,new Pawn(false));
-            board[1][6] = new Square(1,6,new Pawn(false));
-            board[1][7] = new Square(1,7,new Pawn(false));
+            board[1][A] = new Square(1,A,new Pawn(isWhite));
+            board[1][B] = new Square(1,B,new Pawn(isWhite));
+            board[1][C] = new Square(1,C,new Pawn(isWhite));
+            board[1][D] = new Square(1,D,new Pawn(isWhite));
+            board[1][E] = new Square(1,E,new Pawn(isWhite));
+            board[1][F] = new Square(1,F,new Pawn(isWhite));
+            board[1][G] = new Square(1,G,new Pawn(isWhite));
+            board[1][H] = new Square(1,H,new Pawn(isWhite));
 
             //rest of squares have no piece
             for(int i=2; i < 6; i++){
