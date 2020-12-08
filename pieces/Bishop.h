3c1adc2cb6fc4d639301081e98679d50c8e0ff24
Michael James Radey <radeymichael@gmail.com>
2020-12-08 01:19:42 -0500

    initial ui
    connection of c++ exe file and node js code

--- pieces/Bishop.h
+++
@@ -1,9 +0,0 @@
-#include "../Piece.h"
-#include <iostream>
-using namespace std;
-class Bishop : public Piece {
-    public:
-        void toString() {
-            cout << "B";
-        }
-};
--- pieces/Pawn.h
+++
@@ -1,9 +0,0 @@
-#include "../Piece.h"
-#include <iostream>
-using namespace std;
-class Pawn : public Piece {
-    public:
-        void toString() {
-            cout << "P";
-        }
-};
